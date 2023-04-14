598881ad26143b9924961d5becbae98afb5c2fd7
cplazajr <cesarplazajr@gmail.com>
2023-04-13 21:17:32 -0400

    ReviewNotes

---
+++ week3/3.2.1-Arrays.js
@@ -0,0 +1,11 @@
+// create an Array using an Array literal
+let myArray = [1, 2, 3, 4, 5];
+// access the 1st item in the Array
+console.log(myArray[0]);
+// access the last item in the Array
+console.log(myArray[myArray.length - 1]);
+// print the length of the Array
+console.log(myArray.length);
+// use the length property to access the last item in the Array
+
+// with for...of, loop over the Array, modify the value and add to a different Array
